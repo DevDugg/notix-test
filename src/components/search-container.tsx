09e030bc7dcb4b69e6cf29eb0ab2826d61e1c264
DevDugg <mr.aydogdy02@gmail.com>
2025-09-25 14:48:15 +0500

    added debouncedQuery prop to SearchResults

--- src/components/search-container.tsx
+++ src/components/search-container.tsx
@@ -78,6 +78,7 @@ export default function SearchContainer({
         isLoading={isLoading}
         error={error}
         activeIndex={activeIndex}
+        query={debouncedQuery}
       />
     </div>
   );
