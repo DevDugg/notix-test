34d60c4281494a86a4f5f9472d9c26119786e9f2
DevDugg <mr.aydogdy02@gmail.com>
2025-09-25 13:34:58 +0500

    updated search container to handle search params and use useDebounced callback

--- src/components/search-container.tsx
+++ src/components/search-container.tsx
@@ -1,10 +1,37 @@
 "use client";
 
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { usePathname, useRouter } from "next/navigation";
 import SearchInput from "@/components/search-input";
+import { useDebouncedCallback } from "@/hooks/use-debounced-callback";
 
-export default function SearchContainer() {
-  const [searchQuery, setSearchQuery] = useState("");
+export default function SearchContainer({
+  initialQuery,
+}: {
+  initialQuery: string;
+}) {
+  const pathname = usePathname();
+  const { replace } = useRouter();
+  const [inputValue, setInputValue] = useState(initialQuery);
 
-  return <SearchInput value={searchQuery} onChange={setSearchQuery} />;
+  const updateUrl = useDebouncedCallback((query: string) => {
+    const params = new URLSearchParams(window.location.search);
+    if (query) {
+      params.set("q", query);
+    } else {
+      params.delete("q");
+    }
+    replace(`${pathname}?${params.toString()}`);
+  }, 500);
+
+  const handleInputChange = (value: string) => {
+    setInputValue(value);
+    updateUrl(value);
+  };
+
+  useEffect(() => {
+    setInputValue(initialQuery);
+  }, [initialQuery]);
+
+  return <SearchInput value={inputValue} onChange={handleInputChange} />;
 }
